2f603e7d7fad3cd31b6a852bb2bdc86141e285ce
joseu <joseulategomez@gmail.com>
2026-06-12 19:50:23 -0600

    Add 3 sout

---
+++ src/main/java/SistemaSolar/CuerpoCeleste/PlanetaGaseoso.java
@@ -0,0 +1,34 @@
+package SistemaSolar.PlanetaRocoso;
+
+package SistemaSolar.PlanetaRocoso;
+
+import SistemaSolar.CuerpoCeleste.CuerpoCeleste;
+
+public class PlanetaRocoso extends CuerpoCeleste {
+
+    private String composicionCorteza;
+
+    public PlanetaRocoso(String nombre, double tamano, double distanciaAlSol,
+                         int numeroLunas, String composicionCorteza) {
+
+        super(nombre, tamano, distanciaAlSol, numeroLunas);
+
+        if (tamano <= 0 || distanciaAlSol <= 0 || numeroLunas < 0) {
+            throw new IllegalArgumentException("Los valores numéricos no pueden ser inválidos.");
+        }
+
+        if (composicionCorteza == null || composicionCorteza.trim().isEmpty()) {
+            throw new IllegalArgumentException("La composición de la corteza no puede estar vacía.");
+        }
+
+        this.composicionCorteza = composicionCorteza;
+    }
+
+    public String getComposicionCorteza() {
+        return composicionCorteza;
+    }
+
+    public void setComposicionCorteza(String composicionCorteza) {
+        this.composicionCorteza = composicionCorteza;
+    }
+}
--- src/main/java/SistemaSolar/CuerpoCeleste/PlanetaRocoso.java
+++ src/main/java/SistemaSolar/CuerpoCeleste/PlanetaRocoso.java
@@ -1,4 +1,28 @@
-package SistemaSolar.CuerpoCeleste;
+package SistemaSolar.PlanetaGaseoso;
 
-public class PlanetaRocoso {
+import SistemaSolar.CuerpoCeleste.CuerpoCeleste;
+
+public class PlanetaGaseoso extends CuerpoCeleste {
+
+    private boolean tieneAnillos;
+
+    public PlanetaGaseoso(String nombre, double tamano, double distanciaAlSol,
+                          int numeroLunas, boolean tieneAnillos) {
+
+        super(nombre, tamano, distanciaAlSol, numeroLunas);
+
+        if (tamano <= 0 || distanciaAlSol <= 0 || numeroLunas < 0) {
+            throw new IllegalArgumentException("Los valores numéricos no pueden ser inválidos.");
+        }
+
+        this.tieneAnillos = tieneAnillos;
+    }
+
+    public boolean isTieneAnillos() {
+        return tieneAnillos;
+    }
+
+    public void setTieneAnillos(boolean tieneAnillos) {
+        this.tieneAnillos = tieneAnillos;
+    }
 }
